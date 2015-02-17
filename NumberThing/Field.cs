7906b5b32fa6318397534f1152888a45bf2c1f67
etidalgo <etidalgo@sbs.net.nz>
2015-02-17 13:36:57 +1300

    Revert "Update from NumberWhatis. Might have been better to use branching."
    This reverts commit fe7d33864986771bcfd4534a2127a1d2788d4c25.

--- NumberThing/Field.cs
+++ NumberThing/Field.cs
@@ -7,30 +7,72 @@ using System.IO;
 
 namespace NumberThing
 {
+    class Cell
+    {
+        private bool[] candidates;
+
+        private int _value;
+
+        public int value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public string ToString()
+        {
+            return IsSet ? _value.ToString() : " ";
+        }
+
+        public bool IsSet { get { return (_value != 0);  } }
+
+        public Cell(string val = "")
+        {
+            candidates = new bool[NTConstants.FieldSize];
+            _value = !String.IsNullOrEmpty(val) ? int.Parse(val) : 0;
+            Array.ForEach(candidates, n => { n = IsSet; });
+        }
+    }
+
     class Field
     {
         public int[][] field;
+        public Cell[][] Cells;
+
         public Field(StreamReader file)
         {
-            field = new int[NTConstants.Dim][];
+            field = new int[NTConstants.FieldSize][];
             string line;
             int x = 0;
             while ((line = file.ReadLine()) != null)
             {
                 int[] row = line.Split(',').Select(n =>
                     (!String.IsNullOrEmpty(n) ? int.Parse(n) : 0)).ToArray();
-                if (row.Count() < NTConstants.Dim)
+                Cell[] cellRow = line.Split(',').Select(n =>
+                    new Cell(n)).ToArray();
+                if (row.Count() < NTConstants.FieldSize)
                 {
                     int i = row.Count();
-                    Array.Resize(ref row, NTConstants.Dim);
-                    while (i < NTConstants.Dim)
+                    Array.Resize(ref row, NTConstants.FieldSize);
+                    while (i < NTConstants.FieldSize)
                         row[i++] = 0;
+
+                    i = cellRow.Count();
+                    Array.Resize(ref cellRow, NTConstants.FieldSize);
+                    while (i < NTConstants.FieldSize)
+                        cellRow[i++] = new Cell("");
                 }
                 field[x++] = row;
+                Cells[x++] = cellRow;
+
             }
-            while (field.Count() < NTConstants.Dim)
+            while (field.Count() < NTConstants.FieldSize)
             {
-                field[x++] = new int[NTConstants.Dim];
+                field[x++] = new int[NTConstants.FieldSize];
+            }
+            while (Cells.Count() < NTConstants.FieldSize)
+            {
+                Cells[x++] = new Cell[NTConstants.FieldSize];
             }
         }
     }
